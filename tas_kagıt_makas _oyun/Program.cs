17f643abafb9c25af7cd1c55298975eba43cfc2e
seerkanaltun <seerkanaltun@gmail.com>
2022-11-25 12:38:40 +0300

    düzenleme yapıldı .

--- tas_kagıt_makas _oyun/Program.cs
+++ tas_kagıt_makas _oyun/Program.cs
@@ -10,6 +10,75 @@ namespace tas_kagıt_makas__oyun
     {
         static void Main(string[] args)
         {
+            int playerScore = 0, computerScore = 0;
+            string exitKey = "";
+            while (exitKey != "e")
+            {
+                Console.Clear();
+                Console.WriteLine("Rock-Paper-Scissors");
+                Console.WriteLine("Choose [r]ock,[p]aper,[s]cissors or [e]xit");
+                string playerMove = "";
+            GetInput:
+                switch (Console.ReadLine().ToLower())
+                {
+                    case "r":
+                        playerMove = "rock";
+                        break;
+                    case "p":
+                        playerMove = "paper";
+                        break;
+                    case "s":
+                        playerMove = "scissors";
+                        break;
+                    case "e":
+                        exitKey = "e";
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input... Please try again.");
+                        goto GetInput;
+                        break;
+                }
+                Console.WriteLine($"Your choose {playerMove}");
+                Random rnd = new Random();
+                int random = rnd.Next(3); //0,1,2
+                string computerMove = "";
+                switch (random)
+                {
+                    case 0:
+                        computerMove = "rock";
+                        break;
+                    case 1:
+                        computerMove = "paper";
+                        break;
+                    case 2:
+                        computerMove = "scissors";
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine($"Computer's move {computerMove}");
+                if ((playerMove == "rock" && computerMove == "paper") || (playerMove == "scissors" && computerMove == "rock") || (playerMove == "paper" && computerMove == "scissors"))//bilgisayarın kazandığı ihtimaller.et
+                {
+                    computerScore++;
+                    Console.WriteLine("You lost. Computer won.");
+                }
+                else if ((playerMove == "rock" && computerMove == "scissors") || (playerMove == "scissors" && computerMove == "paper") || (playerMove == "paper" && computerMove == "rock"))
+
+                {
+                    playerScore++;
+                    Console.WriteLine("You won. Computer lost.");
+                }
+                else
+                {
+                    Console.WriteLine("Draw / Game is tie.");
+                }
+                Console.WriteLine($"Your score {playerScore} and Computer's score {computerScore}");
+                Console.WriteLine("Press enter to continue");
+
+                Console.ReadKey();
+            }
         }
     }
 }
